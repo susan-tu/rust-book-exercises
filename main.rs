519cdd9aec859ec823552009fa8460a3c7610701
Susan Tu <susantu@Susans-MBP.webpass.net>
2022-01-02 23:04:26 -0800

    mutexes exercise

--- main.rs
+++ main.rs
@@ -13,7 +13,9 @@
 // department or all people in the company by department, sorted alphabetically.
 mod lib;
 use std::collections::HashMap;
-
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     let vec = vec![1, 5, 6, 3, 4, 4];
@@ -40,4 +42,37 @@ fn main() {
     println!("{:?}", assignments.get_by_department(String::from("Product")));
     println!("{:?}", assignments.get_by_department(String::from("Finance")));
     println!("{:?}", assignments.get_all_by_department());
+    intentional_deadlock();
+}
+
+fn intentional_deadlock() {
+    let counter_a = Arc::new(Mutex::new(0));
+    let counter_b = Arc::new(Mutex::new(0));
+    let counter_a1 = Arc::clone(&counter_a);
+    let counter_a2 = Arc::clone(&counter_a);
+    let counter_b1 = Arc::clone(&counter_b);
+    let counter_b2 = Arc::clone(&counter_b);
+    let handle1 = thread::spawn(move || {
+        let mut num_a = counter_a1.lock().unwrap();
+        thread::sleep(Duration::from_millis(100));
+        {
+            let mut num_b = counter_b1.lock().unwrap();
+            *num_b += *num_a;
+
+        }
+        *num_a += 1;
+    });
+
+    let handle2 = thread::spawn(move || {
+        let mut num_b = counter_b2.lock().unwrap();
+        thread::sleep(Duration::from_millis(100));
+        {
+            let mut num_a = counter_a2.lock().unwrap();
+            *num_a += *num_b;
+
+        }
+        *num_b += 1;
+    });
+    handle1.join().unwrap();
+    handle2.join().unwrap();
 }
